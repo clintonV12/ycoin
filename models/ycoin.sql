8da4ef520c97a6c1a3bb6036d9ea33d5ced2ab0f
clinton <vilakaticlinton@gmail.com>
2022-10-05 01:26:58 -0400

    updated db schema

--- models/ycoin.sql
+++ models/ycoin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 22, 2022 at 06:41 PM
+-- Generation Time: Oct 05, 2022 at 07:25 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
@@ -20,6 +20,60 @@ SET time_zone = "+00:00";
 --
 -- Database: `ycoin`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bc_sim_master_accounts`
+--
+
+CREATE TABLE `bc_sim_master_accounts` (
+  `id` int(11) NOT NULL,
+  `total_in_circulation` float DEFAULT NULL,
+  `current_balance` float DEFAULT NULL,
+  `wallet_address` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bc_sim_master_accounts`
+--
+
+INSERT INTO `bc_sim_master_accounts` (`id`, `total_in_circulation`, `current_balance`, `wallet_address`, `createdAt`, `updatedAt`) VALUES
+(2, 1000, 1000, '0000x', '2022-10-04 23:27:20', '2022-10-04 23:27:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bc_sim_user_accounts`
+--
+
+CREATE TABLE `bc_sim_user_accounts` (
+  `id` int(11) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `wallet_address` varchar(200) NOT NULL DEFAULT '0x',
+  `y_balance` float NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bc_sim_user_accounts`
+--
+
+INSERT INTO `bc_sim_user_accounts` (`id`, `phone`, `wallet_address`, `y_balance`, `createdAt`, `updatedAt`) VALUES
+(13, '78679654', '0x', 0, '2022-10-05 02:16:53', '2022-10-05 02:16:53'),
+(14, '78679654', '0x', 0, '2022-10-05 02:17:15', '2022-10-05 02:17:15'),
+(15, '78679654', '0x', 0, '2022-10-05 02:17:31', '2022-10-05 02:17:31'),
+(16, '78679654', '0x', 0, '2022-10-05 02:19:07', '2022-10-05 02:19:07'),
+(17, '78679654', '0x', 0, '2022-10-05 02:19:31', '2022-10-05 02:19:31'),
+(18, '78679654', '0x', 0, '2022-10-05 02:19:53', '2022-10-05 02:19:53'),
+(19, '78679654', '0x', 0, '2022-10-05 02:20:37', '2022-10-05 02:20:37'),
+(20, '78679654', '0x', 0, '2022-10-05 02:21:08', '2022-10-05 02:21:08'),
+(21, '78679654', '0x', 0, '2022-10-05 02:21:52', '2022-10-05 02:21:52'),
+(22, '78679654', '0x', 0, '2022-10-05 02:30:42', '2022-10-05 02:30:42'),
+(23, '78679654', '0x', 0, '2022-10-05 02:32:43', '2022-10-05 02:32:43');
 
 -- --------------------------------------------------------
 
@@ -43,12 +97,33 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `phone`, `wallet_address`, `createdAt`, `updatedAt`) VALUES
-(1, 'clinton', '$2b$10$0y4jfuqoGeFIFJFEVucEFuExF.UCYaWUxg8vphEy2q0DdMnuNbZLu', 'vilakaticlinton@gmail.com', '78679654', '0x', '2022-09-22 14:30:50', '2022-09-20 21:15:56'),
-(3, 'very', '$2b$10$ALe0Xm/FEFt9HYknlhF5sOVzBQjYdPIGrtb.u3EQkI2mv.i6dl4Vm', 'sfg', '364635', '0x', '2022-09-22 14:30:50', '2022-09-22 05:38:16');
+(14, 'clinton', '$2b$10$qDJCE3gLNM5jejHcgSZuf.ovWnF1EqCxqGHtCgP3cO2qRwj7aeXKG', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:16:52', '2022-10-05 02:16:52'),
+(15, 'clinton', '$2b$10$AJXNjjLHmJOQ6dB.RyVaM.o/PLdEMGAW19rg3/H7Th6kMBWyuI.wW', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:17:14', '2022-10-05 02:17:14'),
+(16, 'clinton', '$2b$10$A6p1tkmyCkidmpq00lDUTOh/0qymwF9INXibJlXHbNMLdbmQWjdw.', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:17:31', '2022-10-05 02:17:31'),
+(17, 'clinton', '$2b$10$Pgv0ya7RBba4jugejLj8gOAq125EUxQYQKKrPcKtGOvX0XcEkjPp.', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:19:06', '2022-10-05 02:19:06'),
+(18, 'clinton', '$2b$10$1PlZJETQdhG1Bm30PjjX2eVJXWQI6GODhj3LC8mm.YcbXBNNl3VYq', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:19:29', '2022-10-05 02:19:29'),
+(19, 'clinton', '$2b$10$k0Slzy7NGUTtFKIGhAZr2e.V1FUzNqmkJpUD8khnfJt5RtYBr7nPq', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:19:52', '2022-10-05 02:19:52'),
+(20, 'clinton', '$2b$10$fV/7bVSk8w0vpg/tT717L.Go2ak2q8lfbspFgXusuhKY5R1j0McJG', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:20:37', '2022-10-05 02:20:37'),
+(21, 'clinton', '$2b$10$uJMnVdSyAl2LuWqftpCUAeVU9W6copdBEnSh15hQu/GebTV1UsWjO', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:21:08', '2022-10-05 02:21:08'),
+(22, 'clinton', '$2b$10$97rFA2XSzEYWEqAF6aGIHeifs8zxIvx7UiGpsxtfqVgNJwdSHu5LG', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:21:52', '2022-10-05 02:21:52'),
+(23, 'clinton', '$2b$10$aouIYlhh8iFT2tbz/w3sPeK8mn8vWmS.Zc4JCEddcjQ.QBMo1gn5O', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:30:38', '2022-10-05 02:30:38'),
+(24, 'clinton', '$2b$10$BP7bl58rKt2dSCeF2BLUSuMhZQDF2Covhi/eCixDcl1FLsrkiKWyW', 'vilakaticlinton@yahoo.com.com', '78679654', '0x', '2022-10-05 02:32:42', '2022-10-05 02:32:42');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bc_sim_master_accounts`
+--
+ALTER TABLE `bc_sim_master_accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bc_sim_user_accounts`
+--
+ALTER TABLE `bc_sim_user_accounts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +136,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `bc_sim_master_accounts`
+--
+ALTER TABLE `bc_sim_master_accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `bc_sim_user_accounts`
+--
+ALTER TABLE `bc_sim_user_accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
